df9b1fbfdbf9948e8b2e4ff1e4e7ae781eae9d82
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2020-12-02 02:48:32 -0500

    New files added

---
+++ src/HelloWorldSyntax.java
@@ -0,0 +1,48 @@
+/**
+ * JavaDocs above classes describe the class
+ * and the description has to end with a period.
+ * @author Muhammad B Khan
+ */
+public class HelloWorldSyntax 
+{
+	
+	/*
+    Comment Block starts with /* and ends with * / (no space)
+    all lines inside are comments
+    
+*/
+
+//a single comment line 
+//starts with two forward slashes 
+//the next line is not a comment without //
+
+/*keywords: reserved by Java, highlighted in blue in Netbeans (other colors
+           in other IDEs
+           
+           public ==> anyone can see it/use it
+           private ==> only certain parts of code can see it/use it
+           class ==> an object which can be instantiated (created)
+           new  ==> the word used to instantiate an object
+           static ==> available without instantiation
+*/
+
+/*parts of code syntax:
+*          { ==> left curly brace -- opens a code block
+*          } ==> right curly brace -- closes a code block
+*          " ==> quote -- starts and ends String literals (variables)
+*          [ ==> left bracket -- opens an array and contains size indicator
+*          ] ==> right bracket -- closes an array
+*          ; ==> semi-colon -- ends a directive
+*/
+
+		static void checkAge(int age) {
+			if (age < 18) {
+				System.out.println("Access denied");
+			} else {
+				System.out.println("You are old enough!");
+			}
+		}
+		public static void main(String[] args) {
+			checkAge(20);
+		}
+	}
